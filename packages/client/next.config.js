a380f837b94213222fbeccea948356223143ff6f
Francisco Pessano <franpessano1@gmail.com>
2023-07-08 01:17:57 -0300

    fix: utils in client

--- packages/client/next.config.js
+++ packages/client/next.config.js
@@ -1,10 +1,29 @@
 /** @type {import('next').NextConfig} */
 
+import path from 'path';
+
+const workspace = path.resolve(process.cwd(), '../../');
+
 const nextConfig = {
   compiler: {
     styledComponents: true,
   },
   reactStrictMode: true,
+  webpack: (config, options) => {
+    config.module = {
+      ...config.module,
+      rules: [
+        ...config.module.rules,
+        {
+          exclude: /node_modules/,
+          include: [workspace],
+          test: /\.(js|jsx|ts|tsx)$/,
+          use: options.defaultLoaders.babel,
+        },
+      ],
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
